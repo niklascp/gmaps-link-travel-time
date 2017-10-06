4af82ee64378fd44f8a4619782ade65c00f8d828
niklascp <niklascp@gmail.com>
2017-10-06 14:01:53 +0200

    Initial commit

---
+++ GoogleMaps.py
@@ -0,0 +1,32 @@
+﻿import time
+from datetime import datetime
+import numpy as np
+import pandas as pd
+
+import googlemaps
+
+links = pd.read_csv('data/LinkOriginDestination.csv', sep = ';', names = ['LinkRef', 'FromLat', 'FromLng', 'ToLat', 'ToLng'])
+
+api_key = ''
+with open('api.key', 'r') as file:
+    api_key = file.readline()
+
+gmaps = googlemaps.Client(key=api_key)
+
+with open('data/errors_' + datetime.now().date().isoformat() + '.csv', 'a') as error:
+    with open('data/results_' + datetime.now().date().isoformat() + '.csv', 'a') as output:
+
+        for index, row in links.iterrows():
+            link_ref = row['LinkRef']
+            origins = str(row['FromLat']) + ',' + str(row['FromLng'])
+            destinations = str(row['ToLat']) + ',' + str(row['ToLng'])
+
+            result = gmaps.distance_matrix(origins = origins, destinations = destinations)
+
+            if result['status'] == 'OK':
+                cell = result['rows'][0]['elements'][0]
+                output.write(datetime.now().isoformat() + ',' + link_ref + ',' + str(cell['distance']['value']) + ',' + str(cell['duration']['value']) + '\n')
+            else:
+                error.write(datetime.now().isoformat() + ',' + link_ref + ',' + result['status'])
+                
+            time.sleep(5)
---
+++ README.md
@@ -0,0 +1,15 @@
+# Capturing link travel time using Google
+
+Using the Google Maps distance matrix api.
+
+## Requirements
+
+You will need the Python Client for Google Maps Services:
+
+    $ pip install -U googlemaps
+
+And Pandas
+
+    $ pip install pandas
+
+Please only use in accordance with [Google Maps APIs Terms of Service](https://developers.google.com/maps/terms).
